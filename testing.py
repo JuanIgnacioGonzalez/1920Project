334f50f7a4854fb620af523b6fb332da4df76b10
Tomas Guell <tomasguell123@gmail.com>
2022-06-02 10:05:12 -0300

    funcion add_horizontal_lines

--- testing.py
+++ testing.py
@@ -7,17 +7,22 @@ import plotly.io as pio
 import talib as talib
 import numpy
 
+def add_horizontal_lines(graph, y_low, y_high):
+       graph.add_hline(y=y_high)
+       graph.add_hline(y=y_low)
+
+
 data = pd.read_csv('datos-tipo-cambio-usd-futuro-dolar-frecuencia-diaria.csv', sep = ',')
 
 #print(data.head())
 #print(data.columns)
 
-print(data.index)
-print(data.index[-1])
+#print(data.index)
+#print(data.index[-1])
 
-print(data.columns)
+#print(data.columns)
 
-print(data['tipo_cambio_implicito_en_adrs'])
+#print(data['tipo_cambio_implicito_en_adrs'])
 
 closes = []
 
@@ -28,7 +33,7 @@ while cont <= int(data.index[-1]):
 
 np_closes = numpy.array(closes)
 
-ind_function = getattr(talib, 'MOM')
+ind_function = getattr(talib, 'RSI')
 
 ind = ind_function(np_closes)
 
@@ -40,13 +45,14 @@ print(ind)
 
 
 data1 = data[['indice_tiempo','tipo_cambio_implicito_en_adrs']]
-data1.insert(2,'rsi',ind)
+data1.insert(2,'indicador_elegido',ind)
 plotly_template = pio.templates["plotly_dark"]
 
 try:
        fig = px.line(data1, x = data1.indice_tiempo,y= data1.columns ,title = "DOLAR BLUE")
-       fig.add_hline(y=70)
-       fig.add_hline(y=30)
+       if ind == 'RSI':
+              add_horizontal_lines(fig,30,70)
+       
        fig.update_layout(template=plotly_template)
        fig.show()
 except Exception as e:
